cbb6481bdd545cd819d0332172023bba09b15e83
Rushikesh-24 <rushikeshgaonkar24@gmail.com>
2024-07-08 18:20:56 +0530

    Assignment Updated

---
+++ Sem_3/Assignment1.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+struct Employee
+{
+    string Name;
+    int Id;
+    string Address;
+};
+int main()
+{
+    int number_of_employees, idSearch, flag = 0, a;
+    string nameSearch;
+    cout << endl
+         << "Enter the number of Employees in your company : ";
+    cin >> number_of_employees;
+
+    struct Employee Company1[number_of_employees];
+    cout << endl
+         << "Enter the Details of the Company Employees " << endl
+         << endl;
+    for (int i = 0; i < number_of_employees; i++)
+    {
+        cout << "Enter the Details of Employee " << i << endl;
+        cout << "Name        :";
+        cin >> Company1[i].Name;
+        cout << "Address     :";
+        cin >> Company1[i].Address;
+        cout << "Employee_Id :";
+        cin >> Company1[i].Id;
+        cout << endl;
+    }
+    cout << endl
+         << "Employees Search" << endl
+         << "Enter 1 to search by Employee Id and 2 to search by Address" << endl;
+    cin >> a;
+    switch (a)
+    {
+    case 1:
+        cout << endl
+             << "Enter the Id of the employee you want to search :";
+        cin >> idSearch;
+        for (int i = 0; i < number_of_employees; i++)
+        {
+            if (Company1[i].Id == idSearch)
+            {
+                cout << endl
+                     << "Employee Found" << endl;
+                cout << "Name        :" << Company1[i].Name << endl;
+                cout << "Address     :" << Company1[i].Address << endl;
+                cout << "Employee_Id :" << Company1[i].Id << endl;
+            }
+        }
+        break;
+    case 2:
+        cout << endl
+             << "Enter the Address of the employee you want to search :";
+        cin >> nameSearch;
+        for (int i = 0; i < number_of_employees; i++)
+        {
+            if (Company1[i].Address == nameSearch)
+            {
+                cout << endl
+                     << "Employee Found" << endl;
+                cout << "Name        :" << Company1[i].Name << endl;
+                cout << "Address     :" << Company1[i].Address << endl;
+                cout << "Employee_Id :" << Company1[i].Id << endl;
+            }
+        }
+        break;
+    default:
+        cout << "Invalid choice ";
+        break;
+    }
+    return 0;
+}
---
+++ Sem_3/Assignment4.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"Enter the number of rows and columns in this 2D matrix";
+    cin>>m>>n;
+    if(m!=n){
+        cout<<"Diagonals of only a square matric can be changed";
+        return 0;
+    }
+    int a[n][n];
+    cout<<"Enter the element of the matrix";
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin>>a[i][j];
+        }
+    }
+    cout<<"Before changing"<<endl;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"After chnaging"<<endl;
+    for(int i=0; i<n; i++){
+        int temp = a[i][i];
+        a[i][i] = a[i][n - i - 1];
+        a[i][n - i - 1] = temp;
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
