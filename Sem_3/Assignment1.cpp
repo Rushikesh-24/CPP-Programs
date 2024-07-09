15de8323bd53384a262982dd6333a17d1d930a84
Rushikesh-24 <rushikeshgaonkar24@gmail.com>
2024-07-09 16:12:15 +0530

    Lab Work

--- Sem_3/Assignment1.cpp
+++ Sem_3/Assignment1.cpp
@@ -8,7 +8,7 @@ struct Employee
 };
 int main()
 {
-    int number_of_employees, idSearch, flag = 0, a;
+    int number_of_employees, idSearch, flag = 0;
     string nameSearch;
     cout << endl
          << "Enter the number of Employees in your company : ";
@@ -30,46 +30,54 @@ int main()
         cout << endl;
     }
     cout << endl
-         << "Employees Search" << endl
-         << "Enter 1 to search by Employee Id and 2 to search by Address" << endl;
-    cin >> a;
-    switch (a)
+         << "Employees Search" << endl;
+    int a;
+    do
     {
-    case 1:
-        cout << endl
-             << "Enter the Id of the employee you want to search :";
-        cin >> idSearch;
-        for (int i = 0; i < number_of_employees; i++)
+        cout << "Enter\n1 to search by Employee Id \n2 to search by Address\n3 to exit" << endl;
+        cin >> a;
+        switch (a)
         {
-            if (Company1[i].Id == idSearch)
+        case 1:
+            cout << endl
+                 << "Enter the Id of the employee you want to search :";
+            cin >> idSearch;
+            for (int i = 0; i < number_of_employees; i++)
             {
-                cout << endl
-                     << "Employee Found" << endl;
-                cout << "Name        :" << Company1[i].Name << endl;
-                cout << "Address     :" << Company1[i].Address << endl;
-                cout << "Employee_Id :" << Company1[i].Id << endl;
+                if (Company1[i].Id == idSearch)
+                {
+                    cout << endl
+                         << "Employee Found" << endl;
+                    cout << "Name        :" << Company1[i].Name << endl;
+                    cout << "Address     :" << Company1[i].Address << endl;
+                    cout << "Employee_Id :" << Company1[i].Id << endl;
+                }
             }
-        }
-        break;
-    case 2:
-        cout << endl
-             << "Enter the Address of the employee you want to search :";
-        cin >> nameSearch;
-        for (int i = 0; i < number_of_employees; i++)
-        {
-            if (Company1[i].Address == nameSearch)
+            break;
+        case 2:
+            cout << endl
+                 << "Enter the Address of the employee you want to search :";
+            cin >> nameSearch;
+            for (int i = 0; i < number_of_employees; i++)
             {
-                cout << endl
-                     << "Employee Found" << endl;
-                cout << "Name        :" << Company1[i].Name << endl;
-                cout << "Address     :" << Company1[i].Address << endl;
-                cout << "Employee_Id :" << Company1[i].Id << endl;
+                if (Company1[i].Address == nameSearch)
+                {
+                    cout << endl
+                         << "Employee Found" << endl;
+                    cout << "Name        :" << Company1[i].Name << endl;
+                    cout << "Address     :" << Company1[i].Address << endl;
+                    cout << "Employee_Id :" << Company1[i].Id << endl;
+                }
             }
+            break;
+        case 3:
+            cout << endl
+                 << "Exit";
+        default:
+            cout << "Invalid choice ";
+            break;
         }
-        break;
-    default:
-        cout << "Invalid choice ";
-        break;
-    }
+
+    } while (a != 3);
     return 0;
 }
